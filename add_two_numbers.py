8287a1b6733c9379e8fcdce8eff499a58f622d72
Paul Yeo <kwangupaulyeo@gmail.com>
2016-10-09 05:19:46 -0700

    Solve add two numbers problem

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -5,24 +5,28 @@ class ListNode(object):
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        sum = l1.val + l2.val
-        i = 1
-        while l1.next:
+        sum = 0
+        i = 0
+        while l1:
+            sum += l1.val * (10 ** i)
             l1 = l1.next
-            l2 = l2.next
-            sum += (l1.val + l2.val) * (10**i)
             i += 1
 
-        # length = len(sum) - 1
-        # head = ListNode(sum[length])
-        # current = head
-        # length -= 1
-        # while length >= 0:
-        #     digit = sum[length]
-        #     current.next = ListNode(digit)
-        #     current = current.next
+        i = 0
+        while l2:
+            sum += l2.val * (10 ** i)
+            l2 = l2.next
+            i += 1
 
-        # return head
+        sum = str(sum)
+        length = len(sum) - 1
+        output = []
+        while length >= 0:
+            output.append(int(sum[length]))
+            length -= 1
+        
+        return output
+                    
 
 solution = Solution()
 
@@ -33,5 +37,12 @@ l1.next.next = ListNode(3)
 l2 = ListNode(5)
 l2.next = ListNode(6)
 l2.next.next = ListNode(4)
+
+print(solution.addTwoNumbers(l1, l2))
+
+l1 = ListNode(1)
+l1.next = ListNode(8)
+
+l2 = ListNode(0)
 
 print(solution.addTwoNumbers(l1, l2))
