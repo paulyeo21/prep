6a41327d035807fae4f422ec5129c6256fbc2324
paul.yeo <paul.yeo@workday.com>
2017-09-18 17:41:42 -0700

    Edit distance

--- edit-distance.py
+++ edit-distance.py
@@ -7,27 +7,30 @@
 #    b) Delete a character
 #    c) Replace a character
 #
-# T: O(n*m)
-# S: O(n*m)
+# T: O(m * n)
+# S: O(m)
 
 def min_edit_distance(word1, word2):
     m = len(word1)
     n = len(word2)
-    dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+    dp = [0 for i in range(m + 1)]
 
     for i in range(m + 1):
-        dp[i][0] = i
-    for j in range(n + 1):
-        dp[0][j] = j
+        dp[i] = i
 
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
+    for j in range(1, n + 1):
+        previous = dp[0]
+        dp[0] = j
+        for i in range(1, m + 1):
+            temp = dp[i]
             if word1[i - 1] == word2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1]
+                dp[i] = previous
             else:
-                dp[i][j] = min(dp[i - 1][j - 1] + 1, \
-                           min(dp[i - 1][j] + 1, dp[i][j - 1] + 1))
-    return dp[m][n]
+                dp[i] = min(previous + 1, \
+                        min(dp[i - 1] + 1, dp[i] + 1))
+            previous = temp
+
+    return dp[m]
 
 assert min_edit_distance("a", "") == 1 # delete
 assert min_edit_distance("a", "b") == 1 # replace
